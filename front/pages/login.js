07373cf93f0fd177d1262a8f0335fb7fd62dedac
junho <rkd7997@coinbit.co.kr>
2020-05-21 20:13:13 +0900

    머지

--- front/pages/login.js
+++ front/pages/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, List, Card, Icon } from 'antd';
-import ChartComponent from '../components/ChartComponent';
+// import ChartComponent from '../components/ChartComponent';
+
 import { Col,  Row } from 'antd';
 import LoginForm from '../components/LoginForm';
 
--- front/pages/news.js
+++ front/pages/news.js
@@ -3,17 +3,17 @@ import { Button, List, Card, Icon, Pagination} from 'antd';
 import NicknameEditForm from '../components/NicknameEditForm';
 import Link from 'next/link';
 
-const Announcements = () => {
+const News = () => {
   return (
     <div className="sub_div">
       <div className="sub_menu">
         <ul>
-          <li className="active"><Link href="/announcements">공지사항</Link></li>
-          <li><Link href="/news">소식</Link></li>
+          <li><Link href="/announcements">공지사항</Link></li>
+          <li className="active"><Link href="/news">소식</Link></li>
         </ul>
       </div>
       <div className="sub_page">
-        <h3 className="tit_01">공지사항</h3>
+        <h3 className="tit_01">소식</h3>
         <table className="tb_02" width="100%" >
             <tr>
               <th>번호</th>
@@ -23,7 +23,7 @@ const Announcements = () => {
             </tr>
             <tr>
               <td>1</td>
-              <td>공지사항입니다.</td>
+              <td>뉴스입니다.</td>
               <td>2020-05-21</td>
               <td>100000</td>
             </tr>
@@ -38,4 +38,4 @@ const Announcements = () => {
   );
 };
 
-export default Announcements;
+export default News;
