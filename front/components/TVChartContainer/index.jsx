22bbcfd3b8820e94f1c2080b01477371d3fe6447
nhscoinbit <nhs@coinbit.co.kr>
2020-06-02 16:57:27 +0900

    변경 차트 컬러

--- front/components/TVChartContainer/index.jsx
+++ front/components/TVChartContainer/index.jsx
@@ -31,39 +31,100 @@ export class TVChartContainer extends React.PureComponent {
 
 	componentDidMount() {
 
+		let height = this.props.useDocumentScreenSize ? `${document.documentElement.clientHeight}px` : '100%';
 
+		let volumePaneSize = "small";
+
+		if(this.props.isMobile) {
+			volumePaneSize = "large"
+		}
+
+		let background = "#fff",
+			vertGridProperties_color = "rgb(221, 221, 221)",
+			horzGridProperties_color = "rgb(221, 221, 221)",
+			scalesProperties_textColor = "#4a4a4a",
+			upColor = "#ec5761",
+			downColor = "#0042b7",
+			studiesOverrides = {
+				"volume.volume.color.0": "#0042b7",
+				"volume.volume.color.1": "#ec5761",
+				"volume.volume.transparency": 70
+			},
+			custom_css_url = "custom.css",
+			enabled_features = [
+				'hide_left_toolbar_by_default',
+				// 'remove_library_container_border',
+			],
+			theme = 'Light';
 
 		const widgetOptions = {
 			debug: false,
 			symbol: this.props.symbol,
-			timezone: 'Asia/Seoul',
 			datafeed: Datafeed,
-			interval: this.props.interval,
+			interval: this.props.min || this.props.interval,
 			container_id: this.props.containerId,
+			timezone: 'Asia/Seoul',
 			library_path: this.props.libraryPath,
 			locale: 'ko',
-			disabled_features: ['use_localstorage_for_settings'],
-			enabled_features: ['study_templates'],
+			disabled_features: [
+				'use_localstorage_for_settings',
+			],
+			enabled_features: enabled_features,
+			loading_screen: { backgroundColor: "#ebeff2", foregroundColor: "#0042b7" },
 			charts_storage_url: this.props.chartsStorageUrl,
 			charts_storage_api_version: this.props.chartsStorageApiVersion,
 			client_id: this.props.clientId,
 			user_id: this.props.userId,
 			fullscreen: this.props.fullscreen,
+			custom_css_url: custom_css_url,
+			width: '100%',
+			height: height,
 			autosize: this.props.autosize,
+			time_frames: [
+				// { text: "2y", resolution: "D", description: "2 Years" },
+				// { text: "1y", resolution: "D", description: "1 Year", },
+				// { text: "6month", resolution: "D", description: "6 Month" },
+				// { text: "1month", resolution: "60", description: "1 Month" },
+				// { text: "1week", resolution: "30", description: "1 Week" },
+			],
 			studies_overrides: this.props.studiesOverrides,
-			
+			theme: theme,
 			overrides: {
 				"mainSeriesProperties.showCountdown": true,
-				"paneProperties.background": "#131722",
-				"paneProperties.vertGridProperties.color": "#363c4e",
-				"paneProperties.horzGridProperties.color": "#363c4e",
+				"paneProperties.background": background,
+				"paneProperties.vertGridProperties.color": vertGridProperties_color,
+				"paneProperties.horzGridProperties.color": horzGridProperties_color,
 				"symbolWatermarkProperties.transparency": 90,
-				"scalesProperties.textColor" : "#AAA",
-				"mainSeriesProperties.candleStyle.wickUpColor": '#336854',
-				"mainSeriesProperties.candleStyle.wickDownColor": '#7f323f',
+				"scalesProperties.textColor": scalesProperties_textColor,
+				// "mainSeriesProperties.candleStyle.wickUpColor": '#336854',
+				// "mainSeriesProperties.candleStyle.wickDownColor": '#7f323f',
+				"paneProperties.crossHairProperties.color": "#666",
+				"paneProperties.crossHairProperties.width": 1,
+				"paneProperties.crossHairProperties.style": "solid",
+				//"paneProperties.topMargin": 12,
+				//"paneProperties.bottomMargin": 3,
+				// "mainSeriesProperties.prevClosePriceLineColor": "red",
+				// "mainSeriesProperties.candleStyle.upColor": "#6ba583",
+				// Filled Candle styles
+				"mainSeriesProperties.candleStyle.upColor": upColor,
+				"mainSeriesProperties.candleStyle.downColor": downColor,
+				"mainSeriesProperties.candleStyle.drawBorder": false,
+				"mainSeriesProperties.candleStyle.borderUpColor": upColor,
+				"mainSeriesProperties.candleStyle.borderDownColor": downColor,
+				"mainSeriesProperties.candleStyle.drawWick": true,
+				"mainSeriesProperties.candleStyle.wickUpColor": upColor,
+				"mainSeriesProperties.candleStyle.wickDownColor": downColor,
+				// Hollow Candles styles
+				"mainSeriesProperties.hollowCandleStyle.upColor": upColor,
+				"mainSeriesProperties.hollowCandleStyle.downColor": downColor,
+				"mainSeriesProperties.hollowCandleStyle.drawWick": true,
+				"mainSeriesProperties.hollowCandleStyle.drawBorder": true,
+				"mainSeriesProperties.hollowCandleStyle.borderUpColor": upColor,
+				"mainSeriesProperties.hollowCandleStyle.borderDownColor": downColor,
+				"mainSeriesProperties.hollowCandleStyle.wickUpColor": upColor,
+				"mainSeriesProperties.hollowCandleStyle.wickDownColor": downColor,
 			}
 		};
-
 
 		
 		const tvWidget = new widget(widgetOptions);
@@ -122,7 +183,7 @@ export class TVChartContainer extends React.PureComponent {
 			],
 			theme = 'Light';
         
-        const { tradingPairName } = this.props;
+		const { tradingPairName } = this.props;
 		const widgetOptions = {
 			debug: false,
 			symbol: tradingPairName,
